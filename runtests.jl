d2f61c912b234977f529d423aa61810e738ea7c0
Tim-Gaede <TimGaede@gmail.com>
2019-10-16 02:11:48 -0400

    Add files via upload

---
+++ runtests.jl
@@ -0,0 +1,44 @@
+using Test
+
+include("prime_factors.jl")
+
+# Tests adapted from `problem-specifications//canonical-data.json` @ v1.1.0
+
+primes = Int[]
+
+@testset "No factors" begin
+    @test factors!(1, primes) == []
+end
+
+@testset "Prime number" begin
+    @test factors!(2, primes) == [[2, 1]]
+end
+
+@testset "Square of a prime" begin
+    @test factors!(9, primes) == [[3, 2]]
+end
+
+@testset "Cube of a prime" begin
+    @test factors!(8, primes) == [[2, 3]]
+end
+
+@testset "Plato's number" begin
+    @test factors!(5040, primes) == [[2, 4], [3, 2], [5, 1], [7, 1]]
+end
+
+@testset "Product of primes" begin
+    @test factors!(901_255, primes) == [[5, 1], [17, 1], [23, 1], [461, 1]]
+end
+
+@testset "Factors include a large prime" begin
+    @test factors!(93_819_012_551, primes) == [[11, 1], [9539, 1], [894_119, 1]]
+end
+
+@testset "Test Sieve of Eratosthenes" begin
+    @test sieve(100) == [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43,
+                         47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97]
+end
+
+@testset "Negative number" begin
+    @test_throws DomainError factors!(-42, primes)
+end
---
+++ solution_example.jl
@@ -0,0 +1,70 @@
+#-------------------------------------------------------------------------------
+function factors!(n::Int, primes::Array{Int,1})
+    # Returns a sorted list of all divisors of n.
+    # Will extend the array of primes if possibly inadequate for factorization
+        if n  < 1;    throw(DomainError("n must be greater than zero."));    end
+        if n == 1;    return []; end
+
+
+        res = [] # result
+
+        # Extend the array of primes if it may be inadequate
+        sqrt_n_up = convert(Int64, ceil(√n))
+        if length(primes) ==  0  ||  sqrt_n_up > last(primes)
+            primes′ = sieve(2sqrt_n_up)
+            for i = length(primes) + 1 : length(primes′)
+                push!(primes, primes′[i])
+            end
+        end
+
+        rem = n
+        sqrt_rem_down = convert(Int64, floor(√rem))
+        i = 1
+        while rem ≠ 1  &&  primes[i] ≤ sqrt_rem_down
+            if rem % primes[i] == 0
+                rem ÷= primes[i]
+                pwr = 1
+                while rem % primes[i] == 0
+                    rem ÷= primes[i]
+                    pwr += 1
+                end
+                push!(res, [primes[i], pwr])
+            end
+            sqrt_rem_down = convert(Int64, floor(√rem)) # Speed up or slow down?
+            i += 1
+        end
+
+        if rem ≠ 1;    push!(res, [rem, 1]);    end
+
+
+
+        res # returned
+end
+#-------------------------------------------------------------------------------
+
+
+#-------------------------------------------------------------------------------
+# Implementation of the Sieve of Eratosthenes where we take advantage of
+# the option to iterate a for loop by more than 1.  Multiplying two numbers to
+# get an odd would imply that both of those numbers were also odd.
+function sieve(n::Integer)
+    if n < 2;    return []; end
+    primes = Int64[2]
+    sizehint!(primes, convert( Int64, floor( n / log(n) ) ))
+    oddsAlive = trues((n-1) ÷ 2) # oddsAlive[i] represents 2i + 1
+
+    i_sqrt = (convert( Int64, floor(√n) ) - 1) ÷ 2
+    for i = 1 : i_sqrt
+        if oddsAlive[i] # It's prime.  Kill odd multiples of it
+            push!(primes, 2i + 1)
+            Δᵢ = 2i + 1
+            for iₓ = i+Δᵢ : Δᵢ : length(oddsAlive);   oddsAlive[iₓ] = false; end
+        end
+    end
+    for i = i_sqrt + 1 : length(oddsAlive) # Surviving odds also prime
+        if oddsAlive[i];    push!(primes, 2i + 1); end
+    end
+
+    primes
+end
+#-------------------------------------------------------------------------------

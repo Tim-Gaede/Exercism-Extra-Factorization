2f30f89b144d9fd0b594cf103ccb97cabc28006a
Tim-Gaede <TimGaede@gmail.com>
2019-10-17 04:01:55 -0400

    Update factorization.jl

--- factorization.jl
+++ factorization.jl
@@ -1,15 +1,10 @@
-#-------------------------------------------------------------------------------
-# Returns a sorted list of all divisors of n.
 # Will extend the array of primes if possibly inadequate for factorization
-function factors!(n::Int, primes::Array{Int,1})
+function factoriztion!(n::Int, primes::Array{Int,1})
 
 end
-#-------------------------------------------------------------------------------
 
-
-#-------------------------------------------------------------------------------
+ 
 # Sieve of Eratosthenes
-function sieve(n::Integer)
+function sieve(n::Int)
 
 end
-#-------------------------------------------------------------------------------

a090a80f873a60ea95b93c020e93111bc022c301
Tim-Gaede <TimGaede@gmail.com>
2019-10-24 02:00:56 -0400

    Update prime_factors.jl

--- prime_factors.jl
+++ prime_factors.jl
@@ -1,5 +1,5 @@
 # Will extend the array of primes if possibly inadequate for factorization
-function prime_factors!(n::Int, primes::Array{Int,1})
+function factors!(n::Int, primes::Array{Int,1})
 
 end
 
